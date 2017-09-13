6b7423d5165b0834201479f3a7dcb096b9340265
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2017-09-12 20:24:12 -0500

    remove config files (not need it)

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,17 @@
 use Mix.Config
 
-config :guardian_db, GuardianDb, repo: %{}
+config :guardian, Guardian,
+  issuer: "GuardianDb",
+  secret_key: "woeirulkjosiujgwpeiojlkjw3prowiuefoskjd",
+  serializer: Guardian.TestGuardianSerializer
 
-import_config "#{Mix.env}.exs"
+config :guardian_db, GuardianDb,
+  repo: GuardianDb.Test.Repo
+
+config :guardian_db, ecto_repos: [GuardianDb.Test.Repo]
+
+config :guardian_db, GuardianDb.Test.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  database: "guardian_db_test",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  priv: "priv/test"
