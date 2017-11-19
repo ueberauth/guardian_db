41ad4f243df74d7af13b91ae9a958632fd350edd
Sean Callan <seancallan@gmail.com>
2017-11-19 10:27:12 -0700

    Update GuardianDB to use Guardian v1

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule GuardianDb.Mixfile do
     [app: :guardian_db,
      version: @version,
      description: "DB tracking for token validity",
-     elixir: "~> 1.3 or ~> 1.4 or ~> 1.5",
+     elixir: "~> 1.4 or ~> 1.5",
      elixirc_paths: elixirc_paths(Mix.env),
      package: package(),
      build_embedded: Mix.env == :prod,
@@ -18,18 +18,15 @@ defmodule GuardianDb.Mixfile do
   end
 
   def application do
-    [applications: applications(Mix.env)]
+    [extra_applications: [:logger]]
   end
-
-  defp applications(:test), do: [:postgrex, :ecto, :logger]
-  defp applications(_), do: [:logger]
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_),     do: ["lib"]
 
   defp deps do
-    [{:guardian, ">= 1.0.0-beta.0"},
-     {:ecto, "~> 2.1 or ~> 2.2"},
+    [{:guardian, "~> 1.0"},
+     {:ecto, "~> 2.2"},
      {:postgrex, "~> 0.13", optional: true},
      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
      {:credo, ">= 0.0.0", only: [:dev, :test], runtime: false}]
