b7be0d918b77fc2b0b6846a5dcf07e3982d136ef
Sean Callan <doomspork@users.noreply.github.com>
2021-02-01 14:04:53 -0700

    Add Github Actions for CI (#122)

--- config/config.exs
+++ config/config.exs
@@ -10,4 +10,7 @@ config :guardian_db, ecto_repos: [Guardian.DB.TestSupport.Repo]
 config :guardian_db, Guardian.DB.TestSupport.Repo,
   database: "guardian_db_test",
   pool: Ecto.Adapters.SQL.Sandbox,
-  priv: "priv/temp/guardian_db_test"
+  priv: "priv/temp/guardian_db_test",
+  hostname: Map.get(System.get_env(), "DB_HOST", "localhost"),
+  username: "postgres",
+  password: "postgres"
